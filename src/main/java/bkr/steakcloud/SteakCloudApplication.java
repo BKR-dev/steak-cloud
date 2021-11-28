2b9bb1959f34817b74c7f008a49271e9d79f7d00
BKR <booker@pureops.tech>
2021-11-28 12:33:49 +0100

    spring initializer setup

---
+++ src/main/java/bkr/steakcloud/SteakCloudApplication.java
@@ -0,0 +1,13 @@
+package bkr.steakcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteakCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteakCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bkr/steakcloud/SteakCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package bkr.steakcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteakCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
